8a1ca10fe646fbd7f8981551b72f07a27a13d79b
ATUL6026 <96112121+ATUL6026@users.noreply.github.com>
2022-05-24 18:59:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import os
 from datetime import datetime
 
 path = 'image'
+images = []
+personNames = []
+myList = os.listdir(path)
+print(myList)
+for cu_img in myList:
+    current_Img = cv2.imread(f'{path}/{cu_img}')
+    images.append(current_Img)
+    personNames.append(os.path.splitext(cu_img)[0])
+print(personNames)
