fe0402087c48b9b7cbd2219706a93deb2fa7d540
ATUL6026 <96112121+ATUL6026@users.noreply.github.com>
2022-05-26 08:02:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,8 @@ print(personNames)
 
 def faceEncodings(images):
     encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList;
