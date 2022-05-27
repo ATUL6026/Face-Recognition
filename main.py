d3bd30d6d6612b25a95e882985f503b09409e511
ATUL6026 <96112121+ATUL6026@users.noreply.github.com>
2022-05-27 08:22:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,16 @@ def faceEncodings(images):
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
+
+def attendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            time_now = datetime.now()
+            tStr = time_now.strftime('%H:%M:%S')
+            dStr = time_now.strftime('%d/%m/%Y')
+            f.writelines(f'\n{name},{tStr},{dStr}')
