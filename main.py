e8b4780e47025ce84b9ecdd8ace264cca73f93f5
ATUL6026 <96112121+ATUL6026@users.noreply.github.com>
2022-05-28 09:02:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import face_recognition
 import os
 from datetime import datetime
 
+
 path = 'image'
 images = []
 personNames = []
@@ -15,6 +16,7 @@ for cu_img in myList:
     personNames.append(os.path.splitext(cu_img)[0])
 print(personNames)
 
+
 def faceEncodings(images):
     encodeList = []
     for img in images:
@@ -22,6 +24,7 @@ def faceEncodings(images):
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
+
 
 def attendance(name):
     with open('Attendance.csv', 'r+') as f:
@@ -35,6 +38,42 @@ def attendance(name):
             tStr = time_now.strftime('%H:%M:%S')
             dStr = time_now.strftime('%d/%m/%Y')
             f.writelines(f'\n{name},{tStr},{dStr}')
-            
+
+
 encodeListKnown = faceEncodings(images)
 print('All Encodings Complete!!!')
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("webcam")
+while True:
+    ret, frame = cap.read()
+    print(frame)
+
+    faces = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+    faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+
+    facesCurrentFrame = face_recognition.face_locations(faces)
+    encodesCurrentFrame = face_recognition.face_encodings(faces, facesCurrentFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = personNames[matchIndex].upper()
+            # print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            attendance(name)
+
+    cv2.imshow('Webcam', frame)
+    if cv2.waitKey(1) == 13:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
